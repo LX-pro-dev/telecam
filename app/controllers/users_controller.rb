8cb806fa950ed1f55221dc20da04c7ddc660a68d
LX-pro-dev <lx.pro.dev@gmail.com>
2021-04-16 20:26:32 +0200

    Edit/update user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,13 +7,14 @@ class UsersController < ApplicationController
   end
 
   def update
-    current_user.update(params[:user])
-    redirect_to current_user
+    current_user.update(user_params)
+    redirect_to user_path
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:username, :name, :website, :bio, :email, :phone, :gender)
+    params.require(:user).permit(:username, :name, :website, 
+                                 :bio, :email, :phone, :gender)
   end
 end
